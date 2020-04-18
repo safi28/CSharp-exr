8d4773856c4a6e462406e81158d2602121244927
Saphie <safihanadi28@gmail.com>
2020-04-18 22:25:46 +0300

    oop homework

---
+++ OOP/Homework.1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double l = double.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            double o = double.Parse(Console.ReadLine());
+
+            double garden = n * n;
+            double b = w * l;
+            double wTile = m * o;
+
+            double width = garden - b;
+            double tiles = width / wTile;
+            double time = tiles * 0.2;
+
+            Console.WriteLine($"Needed tiles = {tiles.ToString("N2")}\nTime: {time} min");
+        }
+
+    }
+}
---
+++ OOP/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Person
+        {
+            string Name;
+            int Age;
+
+            public Person(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+
+        }
+
+
+        class Child : Person
+        {
+
+            public Child(string name, int age) : base(name, age)
+            {
+            }
+        }
+
+        public class StartUp
+        {
+            public static void Main()
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+        }
+
+
+    }
+}
+
